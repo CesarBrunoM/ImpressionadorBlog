846ad3dee7502d84b7788fa22ee97ad851cf322d
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-04-16 19:16:28 -0300

    configuração de mensagens de erros no arquivo login.html e configuração de mensagens personalizadas no arquivo forms.py.

--- forms.py
+++ forms.py
@@ -4,15 +4,15 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de usuário', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
-    confirmacao = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('senha')])
+    username = StringField('Nome de usuário', validators=[DataRequired(message='Preenchimento do campo obrigatório.')])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='E-mail inválido.')])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20, message='A senha deve conter de 6 a 20 caracteres.')])
+    confirmacao = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('senha', message='As senhas não conferem.')])
     botao_submit_criarconta = SubmitField('Criar conta')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    email = StringField('E-mail', validators=[DataRequired(), Email('E-mail inválido.')])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20, message='A senha deve conter de 6 a 20 caracteres.')])
     lembrar_dados = BooleanField('Lembrar dados de acesso')
     botao_submit_login = SubmitField('Entrar')
