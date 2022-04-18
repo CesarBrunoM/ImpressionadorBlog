0bc51eef176d7b239ee03f3382414efda2446b89
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-04-18 13:21:46 -0300

    Inicio do Banco de dados
    Inicio da integração com o  banco de dados utilizando flask_sqlalchemy.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -8,7 +9,9 @@ lista_usuarios = ['Bruno', 'Luana', 'Lucas', 'Divino']
 # csrf_token
 
 app.config['SECRET_KEY'] = 'ba803396da27d250f78029201b019681'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
+database = SQLAlchemy(app)
 
 @app.route('/')
 def inicio():
